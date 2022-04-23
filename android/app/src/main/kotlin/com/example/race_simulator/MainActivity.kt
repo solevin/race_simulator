7b1ed8974ecf40e69652e8b60a79cc7a2c63dba2
taiyo78910 <64770666+taiyo78910@users.noreply.github.com>
2022-04-23 13:40:40 +0900

    Revert "Feature/#2"

--- android/app/src/main/kotlin/com/example/race_simulator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.race_simulator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/ui/race/race_view.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class RaceViewModel with ChangeNotifier {
-    var selectedQuestionQuantity = 3;
-    var selectedMemberQuantity = 3;
-
-  void notify() => notifyListeners();
-}
